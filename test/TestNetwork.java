b0127f4b3889b836709e83a6d93954cf827eface
Wybrand van der Meer <wybrandvdmeer@gmail.com>
2017-08-17 19:01:48 +0200

    Better error margin.

--- test/TestNetwork.java
+++ test/TestNetwork.java
@@ -4,7 +4,7 @@ public class TestNetwork {
     @Test
     public void testNetwork() {
         Network network = new Network();
-        network.learn(0.01, 0.01, 0.99, 0.99, 0.5);
+        network.learn(0.01, 0.01, 0.99, 0.99, 0.001);
 
         network.passForward(0.01, 0.01);
         System.out.println(String.format("Output1: %f, output2: %f", network.getOutput(0), network.getOutput(1)));
