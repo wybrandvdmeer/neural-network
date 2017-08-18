cd2032c46426c65f12d68d7326bcc1e710278504
Wybrand van der Meer <wybrandvdmeer@gmail.com>
2017-08-18 14:17:51 +0200

    Scalable network.

--- test/TestNetwork.java
+++ test/TestNetwork.java
@@ -9,4 +9,13 @@ public class TestNetwork {
         network.passForward(0.01, 0.01);
         System.out.println(String.format("Output1: %f, output2: %f", network.getOutput(0), network.getOutput(1)));
     }
+
+    @Test
+    public void testScalableNetwork() {
+        ScalableNetwork network = new ScalableNetwork(2,2,2);
+        network.learn(new double[]{ 0.01, 0.01 }, new double[] { 0.99, 0.99 }, 0.001);
+
+        network.passForward(new double[] { 0.01, 0.01});
+        System.out.println(String.format("Output1: %f, output2: %f", network.getOutput(0), network.getOutput(1)));
+    }
 }
