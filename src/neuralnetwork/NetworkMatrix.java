e2f88294308be8e99c66022fb271cd63e78c4b6f
Wybrand van der Meer <wybrandvdmeer@gmail.com>
2017-09-16 14:46:34 +0200

    Matrix network.

---
+++ src/neuralnetwork/NetworkMatrix.java
@@ -0,0 +1,144 @@
+package neuralnetwork;
+
+import Jama.Matrix;
+
+import java.util.*;
+
+public class NetworkMatrix {
+
+    private double learningConstant = 0.5;
+
+    private final String name;
+
+    private Map<Integer, Matrix> weights = new HashMap<>();
+    private Map<Integer, Matrix> inputs = new HashMap<>();
+    private Map<Integer, Matrix> outputs = new HashMap<>();
+
+    private boolean noTransfer = false;
+
+    public NetworkMatrix(String name, int [] layerSizes) {
+        this.name = name;
+
+        for(int layer=0; layer < layerSizes.length - 1; layer++) {
+            /* rows = neurons, columns = weights
+            */
+            Matrix matrix = new Matrix(layerSizes[layer], layerSizes[layer + 1]);
+            matrix = matrix.random(matrix.getRowDimension(), matrix.getColumnDimension());
+            matrix = matrix.times(2);
+            minus(matrix, 1);
+            weights.put(layer, matrix);
+        }
+    }
+
+    public void setNoTransfer() {
+        noTransfer = true;
+    }
+
+    public void passForward(double [] input) {
+        Matrix inputVector = new Matrix(input, input.length);
+
+        for(int layer = 0; layer < weights.values().size(); layer++) {
+            inputVector = weights.get(layer).times(inputVector);
+            this.inputs.put(layer, inputVector);
+
+            Matrix outputVector = transfer(inputVector);
+            this.outputs.put(layer, outputVector);
+
+            inputVector = outputVector;
+        }
+    }
+
+    private Matrix transfer(Matrix vector) {
+        Matrix transfered = new Matrix(vector.getRowDimension(), 1);
+
+        for(int kol=0; kol < vector.getColumnDimension(); kol++) {
+            for(int row=0; row < vector.getRowDimension(); row++) {
+                if(noTransfer) {
+                    transfered.set(row, kol, vector.get(row, kol));
+                } else {
+                    transfered.set(row, kol, sigmoid(vector.get(row, kol)));
+                }
+            }
+        }
+        return transfered;
+    }
+
+    private double sigmoid(double x) {
+        return (1/( 1 + Math.pow(Math.E,(-1*x))));
+    }
+
+    public double getOutput(int index) {
+        return outputs.get(outputs.size() - 1).get(index,0);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public int learn(double [] inputs, double [] targets, double errorLimit) throws Exception {
+        return learn(inputs, targets, errorLimit, 0);
+    }
+
+    public int learn(double [] inputs, double [] targets, double errorLimit, int maxIterations) throws Exception {
+        int iterations=0;
+        double error=0;
+
+        while(true) {
+            //passForward(inputs);
+
+            // error = error(targets);
+
+            if(error < errorLimit) {
+                break;
+            }
+
+            if(maxIterations > 0 && iterations >= maxIterations) {
+                String s = String.format("Max iterations exceeded for classifier %s.", name);
+                System.out.println(s);
+                return -1;
+            }
+
+            iterations++;
+        }
+
+        return iterations;
+    }
+
+    private void minus(Matrix m, double minus) {
+        for (int i = 0; i < m.getRowDimension(); i++) {
+            for (int j = 0; j < m.getColumnDimension(); j++) {
+                m.set(i, j, m.get(i, j) - minus);
+            }
+        }
+    }
+
+    public void printMatrix(Matrix matrix, String name) {
+        System.out.println("Matrix: " + name);
+        for (int i = 0; i < matrix.getRowDimension(); i++) {
+            for (int j = 0; j < matrix.getColumnDimension(); j++) {
+                String s = String.format("%.2f", matrix.get(i,j));
+                System.out.print(' ');
+                System.out.print(s);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public Map<Integer, Matrix> getWeights() {
+        return weights;
+    }
+}
---
+++ test/neuralnetwork/TestNetworkMatrix.java
@@ -0,0 +1,50 @@
+package neuralnetwork;
+
+import Jama.Matrix;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class TestNetworkMatrix {
+    @Test
+    public void testPassForward() throws Exception {
+        NetworkMatrix network = new NetworkMatrix("test", new int []{2, 2, 2});
+
+        network.getWeights().get(0).set(0,0,1);
+        network.getWeights().get(0).set(0,1,2);
+
+        network.getWeights().get(0).set(1,0,3);
+        network.getWeights().get(0).set(1,1,4);
+
+        network.printMatrix(network.getWeights().get(0), "weight0");
+
+        network.getWeights().get(1).set(0,0,5);
+        network.getWeights().get(1).set(0,1,6);
+
+        network.getWeights().get(1).set(1,0,7);
+        network.getWeights().get(1).set(1,1,8);
+
+        network.printMatrix(network.getWeights().get(1), "weight1");
+
+        network.setNoTransfer();
+
+        network.passForward(new double[] {0.5, 1});
+
+        assertEquals(45.5, network.getOutput(0));
+        assertEquals(61.5, network.getOutput(1));
+
+    }
+
+    public void printMatrix(Matrix matrix, String name) {
+        System.out.println("Matrix: " + name);
+        for (int i = 0; i < matrix.getRowDimension(); i++) {
+            for (int j = 0; j < matrix.getColumnDimension(); j++) {
+                String s = String.format("%.2f", matrix.get(i,j));
+                System.out.print(' ');
+                System.out.print(s);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
